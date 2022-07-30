00e6b30ee588a63945a93f4308972f7cb9d8a7d4
ZeusSR <89997443+ZeusSR@users.noreply.github.com>
2022-07-30 18:21:40 +0530

    Add files via upload

---
+++ PatternOne.java
@@ -0,0 +1,14 @@
+package Ineuron;
+
+public class PatternOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		patternClass obj = new patternClass();
+		obj.PatternIneuron();
+	}
+
+
+	
+	}
