ab502e536d3d8557e7e646cb06ac28d093b1a5b1
ZeusSR <89997443+ZeusSR@users.noreply.github.com>
2022-08-01 17:48:58 +0530

    Add files via upload

---
+++ PatternMainOne_class.java
@@ -0,0 +1,18 @@
+package Ineuron;
+
+public class PatternMainOne_class {
+
+	public static void main(String[] args) {
+
+
+		patternClass obj = new patternClass();
+		obj.PatternIneuron();  //QUESTION 1
+		obj.PatternOneMethod(); //QUESTION 2
+		obj.PatternTwoMethod(); //QUESTION 3
+		obj.PatternThreeMethod(); //QUESTION 4
+		obj.PatternfOURMethod(); //QUESTION 5
+	}
+
+
+	
+	}
