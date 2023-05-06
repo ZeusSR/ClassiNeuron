8267eddbe2615b2edae8c6c957e33cbfc5ad6c7f
ZeusSR <89997443+ZeusSR@users.noreply.github.com>
2023-05-06 16:26:27 +0530

    Add files via upload

---
+++ InnerClasses&FileHandling/src/in/innerClass/Annonyinnerclass_implementinginterface.java
@@ -0,0 +1,24 @@
+package in.innerClass;
+
+/*Here we are seeing an example of annonymous inner class with with interface implementation*/
+public class Annonyinnerclass_implementinginterface {
+
+	public static void main(String[] args) {
+
+		
+		Runnable r = new Runnable() {
+
+			@Override
+			public void run() {
+
+				System.out.println("Inside implemented class");
+				
+			}
+			
+		};
+		
+		Thread t = new Thread(r);
+		t.start();
+	}
+
+}
