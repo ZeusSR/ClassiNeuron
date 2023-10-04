a4f9de6168c28c1e5575636c0be9d075273d9eee
INDRA <89997443+ZeusSR@users.noreply.github.com>
2023-10-04 12:01:44 +0530

    Add files via upload

---
+++ Hibernate/HBDateTimeApp/src/in/neuralnet/model/Person.java
@@ -0,0 +1,80 @@
+package in.neuralnet.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="person_tb")
+public class Person implements Serializable
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer pid;
+	
+	private String pname;
+	
+	private String paddress;
+	
+	private LocalDate dob;
+	private LocalDateTime dom;
+	private LocalTime doj;
+	@Override
+	public String toString() {
+		return "Person [pid=" + pid + ", pname=" + pname + ", paddress=" + paddress + ", dob=" + dob + ", dom=" + dom
+				+ ", doj=" + doj + "]";
+	}
+	public Integer getPid() {
+		return pid;
+	}
+	public void setPid(Integer pid) {
+		this.pid = pid;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public String getPaddress() {
+		return paddress;
+	}
+	public void setPaddress(String paddress) {
+		this.paddress = paddress;
+	}
+	public LocalDate getDob() {
+		return dob;
+	}
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+	public LocalDateTime getDom() {
+		return dom;
+	}
+	public void setDom(LocalDateTime dom) {
+		this.dom = dom;
+	}
+	public LocalTime getDoj() {
+		return doj;
+	}
+	public void setDoj(LocalTime doj) {
+		this.doj = doj;
+	}
+	
+	
+	
+}
